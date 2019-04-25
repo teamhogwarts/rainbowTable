cea78f07c4aedad8c5909247cd373123f55c677f
Benjamin Brodwolf <benjamin.brodwolf@students.fhnw.ch>
2019-04-25 15:38:45 +0200

    Reduktionsfunktion implementiert

--- src/main/java/RainbowTable.java
+++ src/main/java/RainbowTable.java
@@ -1,2 +1,41 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 public class RainbowTable {
+
+    public static void main(String[] args) throws Exception {
+        BigInteger hashValue = MD5("0000000");
+
+        ReductionFunction reductionFunction = new ReductionFunction(7);
+        String result = reductionFunction.executeReductionFunction(hashValue,0);
+        System.out.println(result);
+
+    }
+
+    public static BigInteger MD5(String s) throws Exception {
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(s.getBytes(), 0, s.length());
+        return new BigInteger(1, m.digest());
+    }
+
+//    public static String reduktionsFunktion(BigInteger hashValue, int round) {
+//        BigInteger roundHashValue = hashValue.add(BigInteger.valueOf(round));
+//        char[] z = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+//                'a','b','c','d','e', 'f', 'g', 'h', 'i','j', 'k', 'l',
+//                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+//                'x', 'y', 'z'};
+//        BigInteger[] divideAndRemainders;
+//        StringBuilder builder = new StringBuilder();
+//
+//        for (int i = 1; i <= 7; i++) {
+//            divideAndRemainders = roundHashValue.divideAndRemainder(BigInteger.valueOf(z.length));
+//            roundHashValue = divideAndRemainders[0];
+//            builder.append(z[divideAndRemainders[1].intValue()]);
+//        }
+//
+//        return builder.reverse().toString();
+//    }
 }
+
+
+
---
+++ src/main/java/ReductionFunction.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+
+public class ReductionFunction {
+
+    private char[] z;
+    private int passwordLength;
+
+    public ReductionFunction(int passwordLength) {
+        this.passwordLength = passwordLength;
+        initializePossiblePasswordCharacters();
+    }
+
+    private void initializePossiblePasswordCharacters() {
+        this.z = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a','b','c','d','e', 'f', 'g', 'h', 'i','j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+                'x', 'y', 'z'};
+    }
+
+    public String executeReductionFunction(BigInteger hashValue, int round) {
+        BigInteger roundHashValue = hashValue.add(BigInteger.valueOf(round));
+
+        BigInteger[] divideAndRemainders;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= this.passwordLength; i++) {
+            divideAndRemainders = roundHashValue.divideAndRemainder(BigInteger.valueOf(this.z.length));
+            roundHashValue = divideAndRemainders[0];
+            result.append(this.z[divideAndRemainders[1].intValue()]);
+        }
+
+        return result.reverse().toString();
+    }
+}
