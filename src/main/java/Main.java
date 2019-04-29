8ebe5795aea21c6accbe528a05d1380c77c525c6
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2019-04-29 11:50:48 +0200

    test hinzugefügt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,9 +13,15 @@ public class Main {
         BiMap<String, String > rain = rainbowTable.getRainbowTable();
 
         // rain.keySet().forEach(key -> System.out.println("Key: " + key + " : " + "Value: " + rain.get(key)));
-
+        String hashValue = "1d56a37fb6b08aa709fe90e12ca59e12";
         SearchPassword searchPassword = new SearchPassword(rainbowTable);
+        String password = searchPassword.searchForPassword(new BigInteger(hashValue, 16));
 
-        System.out.println("calculated Password: " + searchPassword.searchForPassword(new BigInteger("1d56a37fb6b08aa709fe90e12ca59e12", 16)));
+        System.out.println("Hashwert: " + hashValue);
+        System.out.println("calculated Password: " + password);
+        System.out.println("passwort gehasht: " + rainbowTable.getHashFunction().MD5(password).toString(16));
+
+
+
     }
 }
